be7f3a1233fb75b88436de7f64efa54aa6055dea
sameh issa <s11925968@gmail.com>
2023-10-24 23:43:25 +0300

    start

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='container'>
